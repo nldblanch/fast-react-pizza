d6dc084f5b7c7c6e5d4164222270b77e968ec37c
Nathan Blanch <nldblanch@gmail.com>
2025-09-12 15:32:55 +0100

    add app layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
