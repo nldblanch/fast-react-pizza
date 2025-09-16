dcb9eef18fa63f9166692fe5d9d5959cb867530e
Nathan Blanch <nldblanch@gmail.com>
2025-09-16 15:35:18 +0100

    implement write for data updating

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
